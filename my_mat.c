869efbced95aa7af6837bb5acacfb5405a138368
ofekrotem400 <92383710+ofekrotem400@users.noreply.github.com>
2021-11-29 19:07:51 +0200

    Final commit

--- my_mat.c
+++ my_mat.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
-#define MIN(X, Y) (((X) < (Y)) ? (X) : (Y))
+#define MIN(X,Y) (((X) < (Y)) ? (X) : (Y))
 #define N 10
+#define Infinity 99999999
 void funcA(int mat[N][N]){
     for (int i = 0; i < N; ++i)
     {
         for (int j = 0; j < N; j++)
         {
-            scanf(" %d",&mat[i][j]);    
+            scanf(" %d",&mat[i][j]);
+            if (mat[i][j] == 0 && i!=j)
+            {
+                mat[i][j]=Infinity;
+            }
+                
         }       
     }
 }
-void funcB(int mat[N][N]){
-    int i;
-    int j;
-    scanf(" %d %d",&i,&j);
-    mat[i][j] != 0 ? printf("True\n") : printf("False\n");
-    
-}
+
 void funcCHelp(int mat[N][N], int pivot){
     for (int i = 0; i < N; ++i)
     {
@@ -42,19 +42,33 @@ void funcC(int mat[N][N]){
     scanf(" %d %d",&iC,&jC);
     for (int i = 0; i < N; i++)
     {
-        funcCHelp(mat,i);        
+        funcCHelp(mat,i);
+    } 
+    if (mat[iC][jC] != 0 && mat[iC][jC] != Infinity)
+    {
+        printf("%d\n",mat[iC][jC]);
     }
-    mat[iC][jC] != 0 ? printf("%d\n",mat[iC][jC]) : printf("%d\n",(-1));
+    else
+    {
+        printf("%d\n",(-1));
+    }
 }
 
-// int main()
-// {
-//     int mat[10][10];
-//     funcA(mat);
-//     funcB(mat);
-//     funcC(mat); 
-//     return 0;
-// }
+void funcB(int mat[N][N]){
+    int i;
+    int j;
+    scanf(" %d %d",&i,&j);
+    for (int i = 0; i < N; i++)
+    {
+        funcCHelp(mat,i);        
+    }
+    if (mat[i][j] != 0 && mat[i][j] != Infinity)
+    {
+        printf("True\n");
+    }
+    else
+    {
+        printf("False\n");
+    }
     
-
-    
+}
